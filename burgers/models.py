8940f8578016938ea6b704778fc6b13943220a05
MadPumpkin3 <fpdjxpa37@gmail.com>
2023-06-18 21:51:06 +0900

    objects 오류

--- burgers/models.py
+++ burgers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Burger(models.Model):
     name = models.CharField(max_length=20)  # CharField은 문자열을 저장하는 명령어
--- config/views.py
+++ config/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from burgers.models import Burger
+
 
 # render함수의 Template의 경로는 config/settings 에서 직접 지정한 TEMPLATES/DIRS에 추가한 디렉터리 경로를 기준으로 한다.
 def main(request):
     # render 함수에서 첫 번째 인수는 request객체, 두 번째 인수는 Templated의 경로를 지정
     return render(request, "main.html")
 
+
 def burger_list(request):
+    burgers = Burger.objects.all()
+    print("전체 햄버거 목록:", burgers)
     return render(request, "burger_list.html")
